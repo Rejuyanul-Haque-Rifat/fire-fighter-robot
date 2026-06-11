3e3103ee0e166b128cb85f77271717122a66e62d
Rejuyanul Haque RiFAT <mdrifat7464@gmail.com>
2026-06-12 00:21:53 +0600

    Add robot control code with smoke and obstacle detection
    Implement a robot control system with smoke detection and navigation features.

---
+++ Robot.ino
@@ -0,0 +1,175 @@
+#include <Servo.h>
+
+Servo scannerServo;
+
+const uint8_t IN1 = 5;
+const uint8_t IN2 = 6;
+const uint8_t IN3 = 7;
+const uint8_t IN4 = 8;
+
+const uint8_t TRIG_PIN = 3;
+const uint8_t ECHO_PIN = 4;
+const uint8_t RELAY_PIN = 10;
+const uint8_t ARM_PIN = 9;
+const uint8_t SMOKE_PIN = A0;
+const uint8_t SERVO_PIN = 11;
+
+const uint16_t SMOKE_THRESHOLD = 400;
+const uint8_t OBSTACLE_DISTANCE = 20;
+const uint32_t PULSE_TIMEOUT = 30000;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(ARM_PIN, OUTPUT);
+  pinMode(SMOKE_PIN, INPUT);
+
+  scannerServo.attach(SERVO_PIN);
+  delay(500);
+  scannerServo.write(90);
+}
+
+uint16_t getDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  unsigned long duration = pulseIn(ECHO_PIN, HIGH, PULSE_TIMEOUT);
+  
+  if (duration == 0) {
+    return 999;
+  }
+  return duration / 58;
+}
+
+uint16_t getSmokeLevel() {
+  uint16_t level = 0;
+  for (uint8_t i = 0; i < 5; i++) {
+    level += analogRead(SMOKE_PIN);
+    delay(5);
+  }
+  return level / 5;
+}
+
+void stopMovement() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void moveForward() {
+  stopMovement();
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void rotateLeft(uint16_t durationMs) {
+  stopMovement();
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  delay(durationMs);
+  stopMovement();
+}
+
+void rotateRight(uint16_t durationMs) {
+  stopMovement();
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  delay(durationMs);
+  stopMovement();
+}
+
+void scanAndNavigate() {
+  scannerServo.write(0);
+  delay(300);
+  uint16_t leftDistance = getDistance();
+
+  scannerServo.write(180);
+  delay(300);
+  uint16_t rightDistance = getDistance();
+
+  scannerServo.write(90);
+  delay(300);
+
+  if (leftDistance < OBSTACLE_DISTANCE && rightDistance < OBSTACLE_DISTANCE) {
+    rotateRight(800);
+  } else if (leftDistance > rightDistance) {
+    rotateLeft(400);
+  } else {
+    rotateRight(400);
+  }
+}
+
+void extinguishFire() {
+  scannerServo.write(0);
+  delay(300);
+  uint16_t leftSmoke = getSmokeLevel();
+
+  scannerServo.write(180);
+  delay(300);
+  uint16_t rightSmoke = getSmokeLevel();
+
+  scannerServo.write(90);
+  delay(300);
+
+  if (leftSmoke > rightSmoke) {
+    rotateLeft(300);
+  } else {
+    rotateRight(300);
+  }
+
+  digitalWrite(ARM_PIN, HIGH);
+  digitalWrite(RELAY_PIN, HIGH);
+  
+  delay(5000);
+
+  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(ARM_PIN, LOW);
+  delay(1000);
+
+  unsigned long waitStart = millis();
+  while (getSmokeLevel() > (SMOKE_THRESHOLD - 50)) {
+    if (millis() - waitStart > 10000) {
+      break;
+    }
+    delay(100);
+  }
+}
+
+void loop() {
+  uint16_t smokeLevel = getSmokeLevel();
+  uint16_t distance = getDistance();
+
+  Serial.print("Smoke: ");
+  Serial.print(smokeLevel);
+  Serial.print(" Distance: ");
+  Serial.println(distance);
+
+  if (smokeLevel > SMOKE_THRESHOLD) {
+    stopMovement();
+    extinguishFire();
+  } else if (distance < OBSTACLE_DISTANCE) {
+    stopMovement();
+    scanAndNavigate();
+  } else {
+    moveForward();
+  }
+}
